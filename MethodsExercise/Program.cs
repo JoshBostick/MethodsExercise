d99ddb7064e630144f6d305f27599989cbf8a404
Josh Bostick <jbostick99@gmail.com>
2023-11-09 11:00:58 -0600

    finished exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine($"{color} is a neat color! What is your favorite animal");
+            var animal = Console.ReadLine();
+            Console.WriteLine($"{animal}'s are awesome! What is your favorite band");
+            var band = Console.ReadLine();
+            Console.WriteLine($"{band}! I love them too!");
         }
     }
 }
