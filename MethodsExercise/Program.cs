63cf00faf318cbda792dd71ef2e580c2272e23ea
Josh Bostick <jbostick99@gmail.com>
2023-11-09 11:11:14 -0600

    fixed exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -8,11 +8,17 @@
             var name = Console.ReadLine();
             Console.WriteLine($"Hello {name}, What is your favorite color?");
             var color = Console.ReadLine();
-            Console.WriteLine($"{color} is a neat color! What is your favorite animal");
+            Console.WriteLine($"{color} is a neat color! What is your favorite animal?");
             var animal = Console.ReadLine();
-            Console.WriteLine($"{animal}'s are awesome! What is your favorite band");
+            Console.WriteLine($"{animal}'s are awesome! What is your favorite band?");
             var band = Console.ReadLine();
             Console.WriteLine($"{band}! I love them too!");
+
+            Console.WriteLine($"Okay {name}, Here is your profile:");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
         }
     }
 }
